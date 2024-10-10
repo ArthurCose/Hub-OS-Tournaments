966963d7fbb7bdf4193f2f16ab1e71ce85cb8a3d
Arthur Cosentino <arthurcose@gmail.com>
2024-10-10 06:31:12 -0400

    Add area grab protection through permaholes to the weakest team

--- mods/tournament/handle_team_disparity.lua
+++ mods/tournament/handle_team_disparity.lua
@@ -1,4 +1,40 @@
 ---@param field Field
+---@param team Team
+---@return Tile?
+local function find_empty_boundary_tile(field, team)
+  local opponent_tile_encountered = false
+  local last_team_tile
+
+  for x = 1, field:width() - 1 do
+    local tile = field:tile_at(x, 1) --[[@as Tile]]
+
+    if tile:team() ~= team then
+      opponent_tile_encountered = true
+
+      if last_team_tile then
+        break
+      end
+    else
+      last_team_tile = tile
+
+      if opponent_tile_encountered then
+        break
+      end
+    end
+  end
+
+  if last_team_tile then
+    for y = 1, field:height() - 1 do
+      local tile = field:tile_at(last_team_tile:x(), y) --[[@as Tile]]
+
+      if not tile:is_reserved() then
+        return tile
+      end
+    end
+  end
+end
+
+---@param field Field
 local function handle_team_disparity(field)
   local teams = { Team.Red, Team.Blue, Team.Other }
   ---@type table<Team, Entity[]>
@@ -16,14 +52,24 @@ local function handle_team_disparity(field)
   end)
 
   -- resolve base count
-  local base_count = math.maxinteger
+  local base_count
+  local equal_teams = true
 
   for _, team in ipairs(teams) do
     local players = team_lists[team]
 
-    if #players > 0 then
-      base_count = math.min(base_count, #players)
+    if base_count and #players ~= base_count then
+      equal_teams = false
     end
+
+    if #players > 0 then
+      base_count = math.min(base_count or math.maxinteger, #players)
+    end
+  end
+
+  if equal_teams then
+    -- teams are equal, nothing to do
+    return
   end
 
   -- try to handle disparity
@@ -31,19 +77,23 @@ local function handle_team_disparity(field)
     local players = team_lists[team]
 
     if #players == base_count then
-      goto continue
+      -- nerf area grabs by opponents using holes
+      local tile = find_empty_boundary_tile(field, team)
+
+      if tile then
+        tile:set_state(TileState.PermaHole)
+      end
+    else
+      -- nerf larger team
+      local raw_multiplier = math.sqrt(base_count / #players)
+      local inverse_multiplier = -math.ceil((1 - raw_multiplier) * 100) / 100
+
+      for _, player in ipairs(players) do
+        player:boost_max_health(player:max_health() * inverse_multiplier)
+        local card_aux = AuxProp.new():increase_card_multiplier(inverse_multiplier)
+        player:add_aux_prop(card_aux)
+      end
     end
-
-    local raw_multiplier = math.sqrt(base_count / #players)
-    local inverse_multiplier = -math.ceil((1 - raw_multiplier) * 100) / 100
-
-    for _, player in ipairs(players) do
-      player:boost_max_health(player:max_health() * inverse_multiplier)
-      local card_aux = AuxProp.new():increase_card_multiplier(inverse_multiplier)
-      player:add_aux_prop(card_aux)
-    end
-
-    ::continue::
   end
 end
 
